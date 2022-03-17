a232e648a527a50562d9d16a1393a05a92d706cf
Krzysztof Dziekiewicz <Krzysztof_Dziekiewicz@epam.com>
2022-03-17 20:39:50 +0100

    Green phase - passing test with an empty job list

--- src/test/java/pl/jenkins/service/JobListServiceTest.java
+++ src/test/java/pl/jenkins/service/JobListServiceTest.java
@@ -1,12 +1,15 @@
 package pl.jenkins.service;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.BDDMockito.given;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+import pl.jenkins.library.JsonList;
+import pl.jenkins.library.JsonMap;
 import pl.jenkins.model.JobStatus;
 
 @ExtendWith(MockitoExtension.class)
@@ -25,6 +28,8 @@ class JobListServiceTest {
     @Test
     void findJobByJiraTicket() {
         // given
+        given(requestService.getJson(DUMMY_URL)).willReturn(fixtureBuildsNumbers("548", "549"));
+
         // when
         JobStatus actualJobStatus = jobListService.getStatusByTicketNumber("ABC-123");
 
@@ -35,10 +40,27 @@ class JobListServiceTest {
     @Test
     void jobListEmpty() {
         // given
+        given(requestService.getJson(DUMMY_URL)).willReturn(fixtureBuildsEmptyList());
+
         // when
         JobStatus actualJobStatus = jobListService.getStatusByTicketNumber("ABC-123");
 
         // then
         assertThat(actualJobStatus).isEqualTo(JobStatus.NOT_FOUND);
+    }
+
+    private JsonMap fixtureBuildsEmptyList() {
+        return fixtureBuildsNumbers();
+    }
+
+    private JsonMap fixtureBuildsNumbers(String... numbers) {
+        JsonMap json = JsonMap.create();
+        JsonList builds = json.addList("builds");
+
+        for (String number : numbers) {
+            builds.addItem("number", number);
+        }
+
+        return json;
     }
 }

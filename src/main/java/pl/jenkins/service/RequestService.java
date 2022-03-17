e982a1b2de00666f2bc63cd77853d3afd3872c45
Krzysztof Dziekiewicz <Krzysztof_Dziekiewicz@epam.com>
2022-03-17 20:53:45 +0100

    Yellow phase - cases for pending tasks visible in html And these are all cases for JobListService

--- src/main/java/pl/jenkins/service/RequestService.java
+++ src/main/java/pl/jenkins/service/RequestService.java
@@ -2,6 +2,9 @@ package pl.jenkins.service;
 
 import pl.jenkins.library.JsonMap;
 
+/**
+ * I do not implement the class and tests as it is not very fascinating for our purposes.
+ */
 public interface RequestService {
 
     JsonMap getJson(String url);

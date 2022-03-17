b411397ac5258689b5e0cd7f4b73c30d4b53df16
Krzysztof Dziekiewicz <Krzysztof_Dziekiewicz@epam.com>
2022-03-17 21:50:21 +0100

    Green phase - JobService - mocked methods

--- src/test/java/pl/jenkins/service/JobServiceTest.java
+++ src/test/java/pl/jenkins/service/JobServiceTest.java
@@ -1,6 +1,7 @@
 package pl.jenkins.service;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.BDDMockito.given;
 
 import java.util.Optional;
 import org.junit.jupiter.api.BeforeEach;
@@ -8,6 +9,7 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+import pl.jenkins.library.JsonMap;
 import pl.jenkins.model.Job;
 import pl.jenkins.model.JobStatus;
 
@@ -27,6 +29,8 @@ public class JobServiceTest {
     @Test
     void jobFound() {
         // given
+        given(requestService.getJson(DUMMY_URL + "/10")).willReturn(fixtureJob("XYZ-123", "SUCCESS"));
+
         // when
         Optional<Job> optionalJob = jobService.getJob("10");
 
@@ -43,5 +47,14 @@ public class JobServiceTest {
 
         // then
         assertThat(optionalJob).isNotPresent();
+    }
+
+    private JsonMap fixtureJob(String ticket, String result) {
+        JsonMap jsonMap = JsonMap.create();
+        JsonMap parameters = jsonMap.addMap("parameters");
+        parameters.addAttribute("branch", ticket);
+        jsonMap.addAttribute("result", result);
+
+        return jsonMap;
     }
 }

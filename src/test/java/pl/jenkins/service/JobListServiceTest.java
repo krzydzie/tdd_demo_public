d6fb4d5d3a47e465204dcd830fee96f2c62e49f2
Krzysztof Dziekiewicz <Krzysztof_Dziekiewicz@epam.com>
2022-03-17 20:49:32 +0100

    Green phase - tests for getStatusByTicketNumber() pass

--- src/test/java/pl/jenkins/service/JobListServiceTest.java
+++ src/test/java/pl/jenkins/service/JobListServiceTest.java
@@ -1,8 +1,12 @@
 package pl.jenkins.service;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.ArgumentMatchers.anyList;
+import static org.mockito.ArgumentMatchers.anyString;
 import static org.mockito.BDDMockito.given;
+import static org.mockito.Mockito.lenient;
 
+import java.util.Optional;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -10,6 +14,7 @@ import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 import pl.jenkins.library.JsonList;
 import pl.jenkins.library.JsonMap;
+import pl.jenkins.model.Job;
 import pl.jenkins.model.JobStatus;
 
 @ExtendWith(MockitoExtension.class)
@@ -32,6 +37,8 @@ class JobListServiceTest {
     void findJobByJiraTicket() {
         // given
         given(requestService.getJson(DUMMY_URL)).willReturn(fixtureBuildsNumbers("548", "549"));
+        given(jobFinder.findByDescription(anyList(), anyString())).willReturn(Optional.of(new Job(JobStatus.RUNNING,
+                "description")));
 
         // when
         JobStatus actualJobStatus = jobListService.getStatusByTicketNumber("ABC-123");
@@ -44,6 +51,9 @@ class JobListServiceTest {
     void jobListEmpty() {
         // given
         given(requestService.getJson(DUMMY_URL)).willReturn(fixtureBuildsEmptyList());
+        //lenient() guardian added, it should not be called. But if starts we will be alarmed as the test starts failing
+        lenient().when(jobFinder.findByDescription(anyList(), anyString()))
+                .thenReturn(Optional.of(new Job(JobStatus.RUNNING, "description")));
 
         // when
         JobStatus actualJobStatus = jobListService.getStatusByTicketNumber("ABC-123");

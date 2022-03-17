990099121d54ab9a6cefa59942d3ca0340cf1f12
Krzysztof Dziekiewicz <Krzysztof_Dziekiewicz@epam.com>
2022-03-17 20:27:51 +0100

    handling case with empty joblist

--- src/main/java/pl/jenkins/service/JobListService.java
+++ src/main/java/pl/jenkins/service/JobListService.java
@@ -1,10 +1,27 @@
 package pl.jenkins.service;
 
+import java.util.List;
+import pl.jenkins.library.JsonMap;
+import pl.jenkins.model.JobNumbers;
 import pl.jenkins.model.JobStatus;
 
 public class JobListService {
 
+    private final String jobsUrl;
+
+    public JobListService(String jobsUrl) {
+        this.jobsUrl = jobsUrl;
+    }
+
     public JobStatus getStatusByTicketNumber(String ticketNumber) {
+        JsonMap json = requestService.getJson(jobsUrl);
+
+        List<String> numbers = new JobNumbers(json).getNumbers();
+
+        if (numbers.isEmpty()) {
+            return JobStatus.NOT_FOUND;
+        }
+
         return JobStatus.PENDING;
     }
 
--- src/test/java/pl/jenkins/service/JobListServiceTest.java
+++ src/test/java/pl/jenkins/service/JobListServiceTest.java
@@ -1,9 +1,7 @@
 package pl.jenkins.service;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.junit.jupiter.api.Assertions.*;
 
-import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import pl.jenkins.model.JobStatus;
@@ -13,7 +11,7 @@ class JobListServiceTest {
 
     @BeforeEach
     void init() {
-        jobListService = new JobListService();
+        jobListService = new JobListService(jobsUrl);
     }
 
     @Test

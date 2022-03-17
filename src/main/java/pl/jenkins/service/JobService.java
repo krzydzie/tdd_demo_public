7ae9d7c935c6fb26972800c3e97b4cf5f9d699e5
Krzysztof Dziekiewicz <Krzysztof_Dziekiewicz@epam.com>
2022-03-17 21:46:22 +0100

    Red phase - JobService - getJob implemented

--- src/main/java/pl/jenkins/service/JobService.java
+++ src/main/java/pl/jenkins/service/JobService.java
@@ -1,11 +1,17 @@
 package pl.jenkins.service;
 
 import java.util.Optional;
+import pl.jenkins.library.JsonMap;
 import pl.jenkins.model.Job;
 
 public class JobService {
 
     public Optional<Job> getJob(String number) {
-        return null;
+        try {
+            JsonMap jsonMap = requestService.getJson(jobsUrl + "/" + jobNumber);
+            return Optional.of(new Job(jsonMap));
+        } catch (Exception e) {
+            return Optional.empty();
+        }
     }
 }

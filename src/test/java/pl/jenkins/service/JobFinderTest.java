bdf8c02651d5e3ccdaba3e5c1149daacdcdce0e3
Krzysztof Dziekiewicz <Krzysztof_Dziekiewicz@epam.com>
2022-03-17 20:48:33 +0100

    skeleton tests for mocked JobFinder.findByDescription() added. We will not forget to test a mocked method later.

---
+++ src/test/java/pl/jenkins/service/JobFinderTest.java
@@ -0,0 +1,25 @@
+package pl.jenkins.service;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+class JobFinderTest {
+
+    @Test
+    void findByDescriptionWhenJobFound() {
+        Assertions.fail("Not implemented yet");
+        // given
+        // when
+        // then
+    }
+
+    @Test
+    void findByDescriptionWhenJobNotFound() {
+        Assertions.fail("Not implemented yet");
+        // given
+        // when
+        // then
+    }
+}

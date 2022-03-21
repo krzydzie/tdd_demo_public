9cf46c9543df793330dbcebeeaeb85cf131537b4
Krzysztof Dziekiewicz <Krzysztof_Dziekiewicz@epam.com>
2022-03-21 12:33:45 +0100

    tagx01 - tests findJobByJiraTicket(), jobListEmpty()

--- src/test/java/pl/jenkins/service/JobListServiceTest.java
+++ src/test/java/pl/jenkins/service/JobListServiceTest.java
@@ -1,5 +1,30 @@
 package pl.jenkins.service;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.junit.jupiter.api.Test;
+import pl.jenkins.model.JobStatus;
+
 class JobListServiceTest {
+
+    @Test
+    void findJobByJiraTicket() {
+        // given
+        // when
+        JobStatus actualJobStatus = jobListService.getStatusByTicketNumber("ABC-123");
+
+        // then
+        assertThat(actualJobStatus).isEqualTo(JobStatus.RUNNING);
+    }
+
+    @Test
+    void jobListEmpty() {
+        // given
+        // when
+        JobStatus actualJobStatus = jobListService.getStatusByTicketNumber("ABC-123");
+
+        // then
+        assertThat(actualJobStatus).isEqualTo(JobStatus.NOT_FOUND);
+    }
     //tdd - here - add initial tests findJobByJiraTicket(), jobListEmpty()
 }

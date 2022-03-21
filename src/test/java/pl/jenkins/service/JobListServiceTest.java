6305e6df0d8915b8e8b72d83b35a6987a2660623
Krzysztof Dziekiewicz <Krzysztof_Dziekiewicz@epam.com>
2022-03-21 12:29:50 +0100

    tag000 - clean up

--- src/test/java/pl/jenkins/service/JobListServiceTest.java
+++ src/test/java/pl/jenkins/service/JobListServiceTest.java
@@ -1,28 +1,4 @@
 package pl.jenkins.service;
 
-import static org.assertj.core.api.Assertions.assertThat;
-import static org.junit.jupiter.api.Assertions.*;
-
-import org.assertj.core.api.Assertions;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
-import pl.jenkins.model.JobStatus;
-
 class JobListServiceTest {
-    private JobListService jobListService;
-
-    @BeforeEach
-    void init() {
-        jobListService = new JobListService();
-    }
-
-    @Test
-    void findJobByJiraTicket() {
-        // given
-        // when
-        JobStatus actualJobStatus = jobListService.getStatusByTicketNumber("ABC-123");
-
-        // then
-        assertThat(actualJobStatus).isEqualTo(JobStatus.RUNNING);
-    }
 }

2cfa7fb77dbd08a6a2624dba0ea672896c0d791c
Krzysztof Dziekiewicz <Krzysztof_Dziekiewicz@epam.com>
2022-03-21 12:42:00 +0100

    tag005 - impl - red - impl JobListService.getStatusByTicketNumber()

--- src/main/java/pl/jenkins/service/JobListService.java
+++ src/main/java/pl/jenkins/service/JobListService.java
@@ -1,10 +1,21 @@
 package pl.jenkins.service;
 
+import java.util.List;
+import pl.jenkins.library.JsonMap;
+import pl.jenkins.model.JobNumbers;
 import pl.jenkins.model.JobStatus;
 
 public class JobListService {
 
     public JobStatus getStatusByTicketNumber(String ticketNumber) {
+        JsonMap json = requestService.getJson(jobsUrl);
+
+        List<String> numbers = new JobNumbers(json).getNumbers();
+
+        if (numbers.isEmpty()) {
+            return JobStatus.NOT_FOUND;
+        }
+
         return JobStatus.PENDING;
     }
 }

90c3903b646a1e3eccc0b0012383c908557563fa
Krzysztof Dziekiewicz <Krzysztof_Dziekiewicz@epam.com>
2022-03-21 12:52:21 +0100

    tagx10 - test - GREEN - passing JobListService jobListEmpty

--- src/test/java/pl/jenkins/service/JobListServiceTest.java
+++ src/test/java/pl/jenkins/service/JobListServiceTest.java
@@ -1,18 +1,19 @@
 package pl.jenkins.service;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.BDDMockito.given;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+import pl.jenkins.library.JsonList;
+import pl.jenkins.library.JsonMap;
 import pl.jenkins.model.JobStatus;
 
 @ExtendWith(MockitoExtension.class)
 class JobListServiceTest {
-    //tdd - RequestService - add method getJson()
-
 
     private static final String DUMMY_URL = "https://jenkins.acme.com/SuperService/job/super-sevice-CI/";
     private JobListService jobListService;
@@ -29,6 +30,8 @@ class JobListServiceTest {
     @Test
     void findJobByJiraTicket() {
         // given
+        given(requestService.getJson(DUMMY_URL)).willReturn(fixtureBuildsNumbers("548", "549"));
+
         // when
         JobStatus actualJobStatus = jobListService.getStatusByTicketNumber("ABC-123");
 
@@ -39,11 +42,27 @@ class JobListServiceTest {
     @Test
     void jobListEmpty() {
         // given
+        given(requestService.getJson(DUMMY_URL)).willReturn(fixtureBuildsEmptyList());
+
         // when
         JobStatus actualJobStatus = jobListService.getStatusByTicketNumber("ABC-123");
 
         // then
         assertThat(actualJobStatus).isEqualTo(JobStatus.NOT_FOUND);
     }
-    //tdd - here - add initial tests findJobByJiraTicket(), jobListEmpty()
+
+    private JsonMap fixtureBuildsEmptyList() {
+        return fixtureBuildsNumbers();
+    }
+
+    private JsonMap fixtureBuildsNumbers(String... numbers) {
+        JsonMap json = JsonMap.create();
+        JsonList builds = json.addList("builds");
+
+        for (String number : numbers) {
+            builds.addItem("number", number);
+        }
+
+        return json;
+    }
 }

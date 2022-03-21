5371e7e79647eb1ee9b8badb8344159586cb00b6
Krzysztof Dziekiewicz <Krzysztof_Dziekiewicz@epam.com>
2022-03-21 17:34:56 +0100

    tag040 - test - GREEN - JobServiceTest getJob passes

--- src/test/java/pl/jenkins/service/JobServiceTest.java
+++ src/test/java/pl/jenkins/service/JobServiceTest.java
@@ -1,6 +1,7 @@
 package pl.jenkins.service;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.BDDMockito.given;
 
 import java.util.Optional;
 import org.junit.jupiter.api.BeforeEach;
@@ -8,6 +9,7 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+import pl.jenkins.library.JsonMap;
 import pl.jenkins.model.Job;
 import pl.jenkins.model.JobStatus;
 
@@ -28,6 +30,8 @@ public class JobServiceTest {
     @Test
     void jobFound() {
         // given
+        given(requestService.getJson(DUMMY_URL + "/10")).willReturn(fixtureJob("XYZ-123", "SUCCESS"));
+
         // when
         Optional<Job> optionalJob = jobService.getJob("10");
 
@@ -46,4 +50,12 @@ public class JobServiceTest {
         assertThat(optionalJob).isNotPresent();
     }
 
+    private JsonMap fixtureJob(String ticket, String result) {
+        JsonMap jsonMap = JsonMap.create();
+        JsonMap parameters = jsonMap.addMap("parameters");
+        parameters.addAttribute("branch", ticket);
+        jsonMap.addAttribute("result", result);
+
+        return jsonMap;
+    }
 }

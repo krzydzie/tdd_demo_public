04ae97d812030521072ce9ccbf29aa5bf6a64da5
Krzysztof Dziekiewicz <Krzysztof_Dziekiewicz@epam.com>
2022-03-21 13:00:26 +0100

    tag016 - test - YELLOW - added tests for mocked methods in JobFinderTest

---
+++ src/test/java/pl/jenkins/service/JobFinderTest.java
@@ -0,0 +1,23 @@
+package pl.jenkins.service;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+class JobFinderTest {
+
+    @Test
+    void findByDescriptionWhenJobFound() {
+        Assertions.fail("Not implemented yet");
+        // given
+        // when
+        // then
+    }
+
+    @Test
+    void findByDescriptionWhenJobNotFound() {
+        Assertions.fail("Not implemented yet");
+        // given
+        // when
+        // then
+    }
+}

6b91f382241184e6b5d9d4ef116a1354e769f348
Krzysztof Dziekiewicz <Krzysztof_Dziekiewicz@epam.com>
2022-03-21 12:47:04 +0100

    tag008 - test - red - filling init - JobListService

--- src/test/java/pl/jenkins/service/JobListServiceTest.java
+++ src/test/java/pl/jenkins/service/JobListServiceTest.java
@@ -4,15 +4,24 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
 import pl.jenkins.model.JobStatus;
 
+@ExtendWith(MockitoExtension.class)
 class JobListServiceTest {
 
+    private static final String DUMMY_URL = "https://jenkins.acme.com/SuperService/job/super-sevice-CI/";
     private JobListService jobListService;
+
+    @Mock
+    private RequestService requestService;
+
 
     @BeforeEach
     void init() {
-        jobListService = new JobListService();
+        jobListService = new JobListService(DUMMY_URL, requestService);
     }
 
     @Test

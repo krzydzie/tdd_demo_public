ff8ffbbcee9e2d587f81f6d287b3519639e7973e
Krzysztof Dziekiewicz <Krzysztof_Dziekiewicz@epam.com>
2022-03-21 17:33:57 +0100

    tag039 - test - YELLOW - JobServiceTest getJob implemented

--- src/test/java/pl/jenkins/service/JobServiceTest.java
+++ src/test/java/pl/jenkins/service/JobServiceTest.java
@@ -5,16 +5,24 @@ import static org.assertj.core.api.Assertions.assertThat;
 import java.util.Optional;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
 import pl.jenkins.model.Job;
 import pl.jenkins.model.JobStatus;
 
+@ExtendWith(MockitoExtension.class)
 public class JobServiceTest {
 
+    private static final String DUMMY_URL = "https://jenkins.acme.com/SuperService/job/super-sevice-CI/";
     private JobService jobService;
+    @Mock
+    private RequestService requestService;
+
 
     @BeforeEach
     void init() {
-        jobService = new JobService();
+        jobService = new JobService(DUMMY_URL, requestService);
     }
 
     @Test

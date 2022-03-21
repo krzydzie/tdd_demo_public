b4ad7002e40a67b8ae57ad85cdbbf2582780ca2e
Krzysztof Dziekiewicz <Krzysztof_Dziekiewicz@epam.com>
2022-03-21 13:21:03 +0100

    tag025 - test - YELLOW - JobFinderTest implemented tests

--- src/test/java/pl/jenkins/service/JobFinderTest.java
+++ src/test/java/pl/jenkins/service/JobFinderTest.java
@@ -1,39 +1,66 @@
 package pl.jenkins.service;
 
-import org.assertj.core.api.Assertions;
+import static java.util.Arrays.asList;
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.util.Optional;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import pl.jenkins.model.Job;
+import pl.jenkins.model.JobStatus;
 
 class JobFinderTest {
 
+    private JobFinder jobFinder;
+
+    @BeforeEach
+    void init() {
+        jobFinder = new JobFinder();
+    }
+
     @Test
     void findByDescriptionWhenJobFound() {
-        Assertions.fail("Not implemented yet");
         // given
         // when
+        Optional<Job> optionalJob = jobFinder.findByDescription(asList("10", "20"), "XYZ-123");
+
         // then
+        Job job = optionalJob.get();
+        assertThat(job.getStatus()).isEqualTo(JobStatus.RUNNING);
+        assertThat(job.getDescription()).contains("XYZ-123");
     }
 
     @Test
     void findByDescriptionWhenJobNotFound() {
-        Assertions.fail("Not implemented yet");
         // given
         // when
+        Optional<Job> optionalJob = jobFinder.findByDescription(asList("10", "20"), "XYZ-123");
+
         // then
+        assertThat(optionalJob).isNotPresent();
     }
 
     @Test
     void foundInHtml() {
-        Assertions.fail("Not implemented yet");
         // given
         // when
+        Optional<Job> optionalJob = jobFinder.findInHtml(
+                "<a tooltip=\"Build Parameters:&lt;br&gt;BRANCH=XYZ-123-some-desc&lt;br&gt\" href=\"#\">", "XYZ-123");
+
         // then
+        assertThat(optionalJob).isPresent();
+        Job job = optionalJob.get();
+        assertThat(job.getStatus()).isEqualTo(JobStatus.PENDING);
+        assertThat(job.getDescription()).contains("XYZ-123");
     }
 
     @Test
     void notFoundInHtml() {
-        Assertions.fail("Not implemented yet");
-        // given
         // when
+        Optional<Job> optionalJob = jobFinder.findInHtml(
+                "<a tooltip=\"Build Parameters:&lt;br&gt;BRANCH=XYZ-124-some-desc&lt;br&gt\" href=\"#\"><a tooltip=\"Build Parameters:&lt;br&gt;BRANCH=XYZ-125-some-desc&lt;br&gt\" href=\"#\">",
+                "XYZ-123");
         // then
+        assertThat(optionalJob).isEmpty();
     }
 }

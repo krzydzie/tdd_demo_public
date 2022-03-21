30a6e6a22e5f952e947f89da5fc68db34b84a964
Krzysztof Dziekiewicz <Krzysztof_Dziekiewicz@epam.com>
2022-03-21 12:39:38 +0100

    tag004 - impl - yellow - added JobListService.getStatusByTicketNumber()

--- src/main/java/pl/jenkins/service/JobListService.java
+++ src/main/java/pl/jenkins/service/JobListService.java
@@ -1,5 +1,10 @@
 package pl.jenkins.service;
 
+import pl.jenkins.model.JobStatus;
+
 public class JobListService {
 
+    public JobStatus getStatusByTicketNumber(String ticketNumber) {
+        return JobStatus.PENDING;
+    }
 }

df06b686a0170b4d8e4ab1ed3d91aaf23cf8b80d
Krzysztof Dziekiewicz <Krzysztof_Dziekiewicz@epam.com>
2022-03-21 13:36:10 +0100

    tag032 - test - YELLOW - added empty tests for JobService

---
+++ src/test/java/pl/jenkins/service/JobServiceTest.java
@@ -0,0 +1,24 @@
+package pl.jenkins.service;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class JobServiceTest {
+
+    @Test
+    void jobFound() {
+        Assertions.fail("Not implemented yet");
+        // given
+        // when
+        // then
+    }
+
+    @Test
+    void jobNotFound() {
+        Assertions.fail("Not implemented yet");
+        // given
+        // when
+        // then
+    }
+
+}

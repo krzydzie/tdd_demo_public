d0afed0e18a375c5b48f1570d23b171c16a8b178
Krzysztof Dziekiewicz <Krzysztof_Dziekiewicz@epam.com>
2022-04-08 15:50:26 +0200

    tagx09 - comment

--- src/main/java/pl/jenkins/service/RequestService.java
+++ src/main/java/pl/jenkins/service/RequestService.java
@@ -3,6 +3,7 @@ package pl.jenkins.service;
 import pl.jenkins.library.JsonMap;
 
 public interface RequestService {
+    //tdd - JobListServiceTest - mock requestService.getJson() in findJobByJiraTicket(), jobListEmpty()
 
     JsonMap getJson(String jobsUrl);
 }

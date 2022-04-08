33a08107b5fdd5cc96e1a870a2da54104ff18e7b
Krzysztof Dziekiewicz <Krzysztof_Dziekiewicz@epam.com>
2022-04-08 15:26:44 +0200

    tagx04 - comment

--- src/main/java/pl/jenkins/service/JobListService.java
+++ src/main/java/pl/jenkins/service/JobListService.java
@@ -3,6 +3,7 @@ package pl.jenkins.service;
 import pl.jenkins.model.JobStatus;
 
 public class JobListService {
+    //tdd - here - implement in getStatusByTicketNumber() not found part
 
     public JobStatus getStatusByTicketNumber(String ticketNumber) {
         return JobStatus.PENDING;
